a512feb24bcb50b3d7c00d8f75a895a5f5c23faa
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-06-15 15:18:36 +0000

    Deploying to gh-pages from @ VladimirPopovich27/Cinemania_3.0@71c863bd36e785455e3c3f59e02d95e19fe26157 🚀

--- src/library.js
+++
@@ -1,7 +0,0 @@
-import './js/header';
-import './js/mobile-menu';
-import './js/themeToggle';
-import './js/modal_movie';
-import './js/card-library';
-import './js/modal-team';
-import './js/scroll-up'
